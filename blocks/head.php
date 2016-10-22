281532a85aa9537308022147e5b53cea5d12b71c
Leor Maizel <leor.maizel@booking.com>
2016-10-21 17:02:35 -0700

    add new favicons, update resume file

--- blocks/head.php
+++ blocks/head.php
@@ -1,10 +1,16 @@
 <meta charset="UTF-8">
 <meta name="description" bcontent="Leor Maizel - UX Designer, Seattle WA">
-<meta property="og:image" content="http://www.leormaizel.com/asssets/img/logo.png"/>
+<meta property="og:image" content="http://www.leormaizel.com/asssets/img/logo-black.png"/>
 <meta property="og:title" content="Leor Maizel - UX Designer, Seattle WA"/>
 <meta property="og:url" content="http://www.leormaizel.com"/>
 <meta property="og:site_name" content="Leor Maizel - UX Design Portfolio"/>
 <meta property="og:type" content="portfolio"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="../css/style.css" />
-<link rel="icon" type="image/ico" href="favicon.ico"/>
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=wAv4baEJex">
+<link rel="icon" type="image/png" href="/favicon-32x32.png?v=wAv4baEJex" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-16x16.png?v=wAv4baEJex" sizes="16x16">
+<link rel="manifest" href="/manifest.json?v=wAv4baEJex">
+<link rel="mask-icon" href="/safari-pinned-tab.svg?v=wAv4baEJex" color="#6e6e6e">
+<link rel="shortcut icon" href="/favicon.ico?v=wAv4baEJex">
+<meta name="theme-color" content="#ffffff">
